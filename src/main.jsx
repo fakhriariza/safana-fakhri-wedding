c600f8afce9002286056f4ceacd34a254c3b0e30
fakhri <arizafakhri@gmail.com>
2025-08-18 15:17:10 +0700

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App.jsx";
 import AppFam from "./AppFam.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <div id="app-wrapper">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/fam/*" element={<AppFam />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
